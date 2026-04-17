1cd788136772b9c4712f3e3867120cf70b0b7d7f
Laxmi Nivas <laxminivas630@gmail.com>
2026-04-17 14:40:24 +0530

    Star and Hash Patterns Today
    codes

---
+++ PatternsPractice3.java
@@ -0,0 +1,16 @@
+package com.javastartpatterns;
+
+public class PatternsPractice3 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= 2; j++) {
+				System.out.print("*#");
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
