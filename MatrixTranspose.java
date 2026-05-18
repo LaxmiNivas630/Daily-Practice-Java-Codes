00ae1c1baac3c3ab898b4236d89bb6015f92d1e0
Laxmi Nivas <laxminivas630@gmail.com>
2026-05-18 22:16:27 +0530

    Matrix Transpose
    By using a 2D array

---
+++ MatrixTranspose.java
@@ -0,0 +1,19 @@
+package com.javalogicalstatements.Arrays;
+
+public class MatrixTranspose {
+
+	public static void main(String[] args) {
+		int a[][] = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+		System.out.println("Transpose Matrix:");
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 2; j++) {
+				System.out.print(a[j][i] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
