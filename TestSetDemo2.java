539c3d8c458316971d72ef83cbb496c03c738f45
Laxmi Nivas <laxminivas630@gmail.com>
2026-06-15 12:30:54 +0530

    Set
    Set in collections

---
+++ TestSetDemo2.java
@@ -0,0 +1,24 @@
+package com.collections;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestSetDemo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Main method started !");
+
+		Set<String> s = new HashSet<>();
+
+		s.add("Sachin");
+		s.add("KOhli");
+		s.add("Dhoni");
+		s.add("Rohit");
+		s.add("Rahul");
+		s.add("Jadeja");
+
+		System.out.println("Main method ended !");
+
+	}
+
+}
