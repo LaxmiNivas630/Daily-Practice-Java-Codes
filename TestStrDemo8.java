9b791c1d106a9955f57a3818dc24df479abc5095
Laxmi Nivas <laxminivas630@gmail.com>
2026-06-30 14:21:59 +0530

    Strings
    String handling using the methods

---
+++ TestStrDemo8.java
@@ -0,0 +1,27 @@
+package com.stringhandling;
+
+import java.util.Scanner;
+
+public class TestStrDemo8 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your username : ");
+
+		String username = sc.next();
+
+		System.out.println("Enter your password :");
+		String password = sc.next();
+
+		if (username.equalsIgnoreCase("admin") && password.equals("Admin@123")) {
+			System.out.println("Welcome to Home Page !!");
+
+		}else {
+			System.err.println("Something went wrong or Invalid Credentials");
+		}
+		sc.close();
+
+	}
+	
+
+}
